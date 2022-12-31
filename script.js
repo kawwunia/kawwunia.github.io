4fb5c88a27d2cf3cdce3fef60ab3aa089f5aff1c
kawwunia <106489995+kawwunia@users.noreply.github.com>
2022-12-31 16:56:56 +0100

    Update script.js
    commetns

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-var canvas = document.getElementById("canva");
-var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height/2;
-var ballRadius = 10;
-var dx = 2;
-var dy = 2;
-var color = "red";
+var canvas = document.getElementById("canva"); //get canvas as variable
+var ctx = canvas.getContext("2d"); //set canvas
+//Ball should be changed to object later $
+var x = canvas.width/2; //ball's x
+var y = canvas.height/2; //ball's y
+var ballRadius = 10; //ball's radius
+var dx = 2; //Movement speed x
+var dy = 2; //Movement speed y
+var color = "red"; //balls color
+//Up
 var rightPressed = false;
 var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
+//text color
 var text_color = "#000";
 
-//Czas
-var wynik = 0;
-var timer = 50; //def 50
-var czas = timer;
-var czas_powerup = 0;
-var def_roznicaCzasu = 0.1;
-var spow_roznicaCzasu = 0.05;
+//Time and score
+var wynik = 0; //score
+var timer = 5000000; //def 50
+var czas = timer; //set timer
+var czas_powerup = 0; //powerup timer
+var def_roznicaCzasu = 0.1; //default timer update
+var spow_roznicaCzasu = 0.05; //powerup timer update
 
-var point = [20,15,7];
-var powerup = {x:0,y:0,radius:7,color:"blue",type:0,active:false};
-const bttn_left = document.getElementById("b_l");
-const bttn_up = document.getElementById("b_u");
-const bttn_down = document.getElementById("b_d");
-const bttn_right = document.getElementById("b_r");
+//powerup
+var activePowerup = 0; //0 = slower time (blue timer), 1 = double the points (orange ball) 
+
+var point = [20,15,7]; //point array
+var powerup = {x:0,y:0,radius:7,color:"blue",type:0,active:false}; //powerup object
+const bttn_left = document.getElementById("b_l"); //get html button left
+const bttn_up = document.getElementById("b_u"); //get html button up
+const bttn_down = document.getElementById("b_d"); //get html button down
+const bttn_right = document.getElementById("b_r"); //get html button right
 
 
-function keyDownHandler(e) {
+function keyDownHandler(e) { //Key Handlers (Down)
   if (e.key === "Right" || e.key === "ArrowRight" || e.keyCode == 68) {
     rightPressed = true;
   }
@@ -43,7 +49,7 @@ function keyDownHandler(e) {
   }
 }
 
-function keyUpHandler(e) {
+function keyUpHandler(e) { //Key Handlers (Up)
   if (e.key === "Right" || e.key === "ArrowRight" || e.keyCode == 68) {
     rightPressed = false;
   }
@@ -58,14 +64,14 @@ function keyUpHandler(e) {
   }
 }
 
-function drawBall() {
+function drawBall() { //draw Ball
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = color;
+    ctx.fillStyle = color; //set ball's color
     ctx.fill();
     ctx.closePath();
 }
-function drawPoint() {
+function drawPoint() { //draw Point 
     ctx.beginPath();
     ctx.arc(point[0], point[1], point[2], 0, Math.PI*2);
     ctx.fillStyle = "gold";
@@ -75,8 +81,8 @@ function drawPoint() {
     ctx.closePath();
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function draw() { //main Event (refresh time 10)
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear 
     drawBall();
     drawPoint();
     Movement();
@@ -85,37 +91,38 @@ function draw() {
     drawPowerUp();
     CollisionDetect();
 }
-function drawScore() {
+function drawScore() { //draw Score as text
   ctx.font = "16px Arial";
   ctx.fillStyle = "#000";
   ctx.fillText(`Wynik: ${wynik}`, 8, 20);
 }
-function drawTime() {
-  if(czas_powerup <= 0) {
-  czas -= def_roznicaCzasu;
-  text_color = "#000"
+function drawTime() { //draw Timer (black = no active effects, blue = time is slower)
+  //Powerup affects on time
+  if(czas_powerup <= 0 && activePowerup == 0) { //add activePowerup $
+  czas -= def_roznicaCzasu; //timer update if powerup is not active
+  text_color = "#000" //change color to default (black)
   }
   else {
-    czas -= spow_roznicaCzasu;
-    text_color = "blue";
+    czas -= spow_roznicaCzasu; //timer update if powerup is active
+    text_color = "blue"; //change text color to blue if powerup is not active
   }
-czas_powerup -= 0.1;
-  ctx.font = "16px Arial";
-  ctx.fillStyle = text_color;
-  ctx.fillText(`Czas: ${(czas/10).toFixed(2)}`, 8, 50);
-  if (czas <= 0) {
+czas_powerup -= 0.1; //Update powerup time
+  ctx.font = "16px Arial"; //set font and size
+  ctx.fillStyle = text_color; //set color
+  ctx.fillText(`Czas: ${(czas/10).toFixed(2)}`, 8, 50); //Set text
+  if (czas <= 0) { //Game over event
     GameOver();
   }
 }
 
 function GameOver() {
-clearInterval(interval);
-if (localStorage.getItem("highscore") < wynik) {
+clearInterval(interval); //stop the game
+if (localStorage.getItem("highscore") < wynik) { //update highscore
   localStorage.setItem("highscore", wynik);
 }
-const alertText = "Koniec! \nWynik: " + wynik + "\nNajlepszy wynik: " + localStorage.getItem("highscore");
-alert(alertText);
-location.reload();
+const alertText = "Koniec! \nWynik: " + wynik + "\nNajlepszy wynik: " + localStorage.getItem("highscore"); //set alert text
+alert(alertText); //alert
+location.reload(); //reload the page
 }
 function Movement() {
     //Movement
@@ -131,7 +138,7 @@ function Movement() {
     if(downPressed) {
         y += dy;
     }
-    //Walls
+    //Border
     if(x + ballRadius < 0) {
         x = 0;
     }
@@ -144,28 +151,28 @@ function Movement() {
     else if(y - ballRadius > canvas.height) {
         y = canvas.height;
     }
-//przycisk dol
+//button go down
 bttn_down.onpointerdown = function() {
   downPressed = true;
 }
 bttn_down.onpointerup = function() {
   downPressed = false;
 }
-//przycisk gora
+//button go up
 bttn_up.onpointerdown = function() {
   upPressed = true;
 }
 bttn_up.onpointerup = function() {
   upPressed = false;
 }
-//przycisk lewo
+//button go left
 bttn_left.onpointerdown = function() {
   leftPressed = true;
 }
 bttn_left.onpointerup = function() {
   leftPressed = false;
 }
-//przycisk prawo
+//button go right
 bttn_right.onpointerdown = function() {
   rightPressed = true;
 }
@@ -178,9 +185,9 @@ bttn_right.onpointerup = function() {
 function CollisionDetect() {
   //Collision with point
     if((x >= point[0] && x <= point[0] + point[2] && y >= point[1] && y <= point[1] + point[2]) || (x <= point[0] && x >= point[0] - point[2] && y <= point[1] && y >= point[1] - point[2])) {
-        wynik += 1;
-        czas = timer;
-        point[0] = GetRandom(canvas.width - 10);
+        wynik += 1; //Add point
+        czas = timer; //set timer
+        point[0] = GetRandom(canvas.width - 10); //set powerup location
         point[1] = GetRandom(canvas.height - 10);
         if (powerup.active == false) {
         SpawnPowerUp();
@@ -189,13 +196,14 @@ function CollisionDetect() {
   //Collision with PowerUp
   if(((x >= powerup.x && x <= powerup.x + powerup.radius && y >= powerup.y && y <= powerup.y + powerup.radius) || (x <= powerup.x && x >= powerup.x - powerup.radius && y <= powerup.y && y >= powerup.y - powerup.radius)) && powerup.active == true) {
 powerup.active = false;
-czas_powerup = 50;
+czas_powerup = 50; //set powerup timer
+activePowerup = powerup.type; //set powerup type
 }
 }
 
-function SpawnPowerUp() {
+function SpawnPowerUp() { //spawn powerup
 
-if((Math.round(GetRandom(100)) % 2 == 0)) {
+if((Math.round(GetRandom(100)) % 2 == 0)) { //Spawn if rng is odd or even
   powerup.active = true;
   powerup.x = GetRandom(canvas.width - 10);
   powerup.y = GetRandom(canvas.height - 10);
@@ -203,25 +211,27 @@ if((Math.round(GetRandom(100)) % 2 == 0)) {
 
 }
 
-function drawPowerUp() {
+function drawPowerUp() { //draw power up
   ctx.beginPath();
-  if(powerup.active == true) {
+  if(powerup.active == true) { //draw if powerup is active
   ctx.arc(powerup.x, powerup.y, powerup.radius, 0, Math.PI*2);
-  ctx.fillStyle = powerup.color;
+  ctx.fillStyle = powerup.color; //powerup color based on which powerup should be working (powerup.type))
   ctx.fill();
-  ctx.strokeStyle = "#000";
+  ctx.strokeStyle = "#000"; //do not change 
   ctx.stroke();
 }
   ctx.closePath();
 }
 
-function GetRandom(max) {
+function GetRandom(max) { //rng, not working properly
     return Math.random() * (max - 10) + 10;
 }
 
+//Event listeners for buttons
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("keydown", keyDownHandler, false);
 
+//intervals and point location rng
 const interval = setInterval(draw, 10);
 point[0] = GetRandom(canvas.width - 10);
 point[1] = GetRandom(canvas.height - 10);
